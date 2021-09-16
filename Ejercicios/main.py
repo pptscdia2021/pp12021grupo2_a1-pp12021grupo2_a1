6923e3cc798f5b76b8e1ceb60d5a93bfb65475aa
Mancuso <mancuso.lobo@gmail.com>
2021-09-16 13:58:30 -0300

    "Retoques finales postas (?"

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -7,6 +7,6 @@ operaciones = {'suma' : suma, 'resta' : resta, 'producto' : producto}
 valores = []
 
 if __name__ == "__main__":
-    opcion = int(input('Operacion:\n1- {0}\n2- {1}\n3- {2}\nSeleccione: '.format(*list(operaciones.keys()))))
-    valores.extend(map(int, list(input('Ingrese valores separados por espacio: ').split())))
+    opcion = int(input('Operacion:\n1- {0}\n2- {1}\n3- {2}\nSeleccione: '.format(*map(lambda x: x.capitalize(),list(operaciones.keys())))))
+    valores.extend(map(int, list(input('Ingrese 2 valores separados por espacio: ').split())))
     imprimir(list(operaciones.keys())[opcion-1].capitalize(), operaciones[list(operaciones.keys())[opcion-1]](*valores))
