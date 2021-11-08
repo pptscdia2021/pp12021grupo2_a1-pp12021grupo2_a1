4ef36d5bbc7c1f8a024124d44dce7b3c07dcf852
Mancuso <mancuso.lobo@gmail.com>
2021-11-07 23:17:57 -0300

    POO retoques

--- Practico 2/POO/POO_investpy.py
+++ Practico 2/POO/POO_investpy.py
@@ -1,20 +1,40 @@
-
 import investpy
 import pandas as pd
 
 class Investpy:
-    def __init__(self, country):
-     self.country = country
-     self.as_json= False
+  def __init__(self, stocks):
+     self.country = 'spain'
+     self.as_json= True
      self.order = 'ascending'
      self.interval = 'Daily'
-     self.df = investpy.stocks.get_stocks_overview(country=self.country, as_json = self.as_json , n_results = 35)
-     del(self.df['country'])
+     self.stocks = list(stocks.split())
 
-    def toDF(self):
-        return self.df
+     listatickers = list()
+     for ticker in self.stocks:
+       data = pd.DataFrame(investpy.stocks.get_stock_recent_data(stock=ticker, country=self.country)).tail(1)
+       data['Ticker'] = ticker.upper()
+       listatickers.append(data)
+     self.df = pd.concat(listatickers) 
+     self.df = self.df.reset_index()
+     columnsName = ["Fecha", "Abierto", "Maximo", "Minimo", "Cierre", "Volumen", "Moneda", "Accion"]
+     self.df.columns = columnsName
 
-ipy = Investpy('spain')
-ipy.toDF()
+  def toDF(self):
+    return self.df
 
+  def toCSV(self):
+    return self.df.to_csv('investypy.csv')
 
+  def maximos(self, columna, cantidad):
+    '''Devuelve la cantidad de maximos indicado del DataFrame indicando la columna'''
+    resultado = self.df.sort_values(columna, ascending=False)
+    resultado = resultado.head(cantidad).to_dict('records')
+    resultado = list(map(lambda x: 'Ganancia maxima del día de {0}: {1}'.format(x['Accion'], x[columna]), resultado))
+    return '\n'.join(resultado)
+
+  def minimos(self, columna, cantidad):
+    '''Devuelve la cantidad de minimos indicado del DataFrame indicando la columna'''
+    resultado = self.df.sort_values(columna)
+    resultado = resultado.head(cantidad).to_dict('records')
+    resultado = list(map(lambda x: 'Mayor perdida del día de {0}: {1}'.format(x['Accion'], x[columna]), resultado))
+    return '\n'.join(resultado)
--- Practico 2/POO/POO_webscrapin.py
+++ Practico 2/POO/POO_webscrapin.py
@@ -6,7 +6,10 @@ class webscraping:
         self.url = url
         self.path = 'DatosWebscraping.csv'
         self.table_id = table
-        self.df = pd.read_html(url, attrs=self.table_id, thousands='.', decimal=',')[0]
+        self.df = pd.read_html(url, attrs=self.table_id, thousands='.', decimal=',', flavor=None)[0]
+        self.df = self.df.drop(['Efectivo (miles €)', 'Hora'], axis=1)
+        columnsName = ["Accion", "Cierre", "Diferencia", "Maximo", "Minimo", "Volumen", "Fecha"]
+        self.df.columns = columnsName
 
     def toCSV(self):
       '''Exporta datos en formato CSV.'''
@@ -19,9 +22,13 @@ class webscraping:
     def maximos(self, columna, cantidad):
       '''Devuelve la cantidad de maximos indicado del DataFrame indicando la columna'''
       resultado = self.df.sort_values(columna, ascending=False)
-      return resultado.head(cantidad)
+      resultado = resultado.head(cantidad).to_dict('records')
+      resultado = list(map(lambda x: 'Ganancia maxima del día de {0}: {1}'.format(x['Accion'], x[columna]), resultado))
+      return '\n'.join(resultado)
 
     def minimos(self, columna, cantidad):
       '''Devuelve la cantidad de minimos indicado del DataFrame indicando la columna'''
       resultado = self.df.sort_values(columna)
-      return resultado.head(cantidad)
+      resultado = resultado.head(cantidad).to_dict('records')
+      resultado = list(map(lambda x: 'Mayor perdida del día de {0}: {1}'.format(x['Accion'], x[columna]), resultado))
+      return '\n'.join(resultado)
