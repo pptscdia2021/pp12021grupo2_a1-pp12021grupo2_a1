9b29617e09af6704c889c69bc6d321ab101f234f
Mancuso <mancuso.lobo@gmail.com>
2021-11-08 15:24:29 -0300

    main completo

---
+++ Practico 2/POO/POO_main.py
@@ -0,0 +1,32 @@
+from POO_webscrapin import webscraping as ws
+from POO_yFinance import yFinance as yf
+from POO_investpy import investPy as inv
+from POO_graficos import Graficos as gf
+import pandas as pd
+
+if __name__ == "__main__":
+  url = "https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
+  table = {"id":"ctl00_Contenido_tblAcciones"}
+  bolsaMadrid = ws(url, table)
+
+  webscrap = ws(url, table)
+  data = webscrap.toDF()
+  tickers = 'BBVA TELEFONICA B.SANTANDER'
+  df_list = list()
+  for ticker in list(tickers.split()):
+    resultado = data.where(data.Accion == ticker)
+    resultado = resultado.dropna()
+    df_list.append(resultado)
+
+  data = pd.concat(df_list)
+
+  tickers2 = 'bbva.mc tef.mc san.mc'
+  data_yf = yf(tickers2.upper())
+  data2 = data_yf.toDF()
+
+  data_inv = inv(tickers2.replace('.mc', ''))
+  data3 = data_inv.toDF()
+
+  df_dict = {'Webscraping':data, 'yFinance':data2, 'InvestPy':data3}
+  grafic = gf(df_dict)
+  grafic.dibujar(12)
